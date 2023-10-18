62e730877d22a16540d9f5858b3d7fea27e5cc8c
solaleh81 <solalehalizadeh81.com>
2023-10-18 12:46:43 +0330

    chaadding contact url.

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path
 
 from . import views
-from .views import index, contact
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('contact/', views.contact, name="contact"),
+
 ]
