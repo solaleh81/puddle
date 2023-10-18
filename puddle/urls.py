54692c560dc3c9d127b71f89703ebe2a718f5591
solaleh81 <solalehalizadeh81.com>
2023-10-18 17:39:36 +0330

    adding static url.

--- puddle/urls.py
+++ puddle/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 
 from core.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("core.urls")),
-]
+    path('category/', include("item.urls")),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
