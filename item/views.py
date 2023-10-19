f1a0963fdb7a612fc3a4cb13026074dc7e8b635c
solaleh81 <solalehalizadeh81.com>
2023-10-19 22:55:25 +0330

    adding related_items.

--- item/views.py
+++ item/views.py
@@ -7,6 +7,6 @@ from .models import Category, Item
 
 def detail(request, id):
     item = get_object_or_404(Item, pk=id)
-    print(item.name)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=id)[0:3]
 
-    return render (request, 'item/item_detail.html', context={'item': item, 'category': item.category})
+    return render (request, 'item/item_detail.html', context={'item': item, 'category': item.category, 'related_items': related_items})
