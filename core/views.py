d9684b6f7d482ba2a32579e62ef6719f9d3aff18
solaleh81 <solalehalizadeh81.com>
2023-10-20 10:59:10 +0330

    adding def signup view.

--- core/views.py
+++ core/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from item.models import Category, Item
 
+from .forms import SignUpForm
+
+
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
-    return render(request, 'core/index.html', context={'categories': categories, 'items': items})
+    return render(
+        request, "core/index.html", context={"categories": categories, "items": items}
+    )
+
 
 def contact(request):
-    return render(request, 'core/contact.html')
+    return render(request, "core/contact.html")
+
+
+def signup(request):
+    form = SignUpForm()
+
+    return render(request, "core/signup.html", context={"form": form})
