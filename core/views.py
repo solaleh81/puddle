dd67b008760e45a1d11a2322542862beed0a3961
solaleh81 <solalehalizadeh81.com>
2023-10-20 11:43:34 +0330

    adding post request for def signup.

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from item.models import Category, Item
 
 from .forms import SignUpForm
@@ -17,6 +17,13 @@ def contact(request):
 
 
 def signup(request):
-    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect("core:login")
+    else:
+        form = SignUpForm()
 
     return render(request, "core/signup.html", context={"form": form})
