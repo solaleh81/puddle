a1be01bcde5df9733dc0bde0fdaf5ca41cd9d79d
solaleh81 <solalehalizadeh81.com>
2023-10-21 12:00:50 +0330

    adding new item url.

--- item/urls.py
+++ item/urls.py
@@ -4,9 +4,9 @@ from django.urls import path, include
 from . import views
 from core.views import index
 
-app_name = 'item'
+app_name = "item"
 
 urlpatterns = [
-    path('<id>/', views.detail, name="detail"),
-
+    path("new/", views.new, name="new"),
+    path("<id>/", views.detail, name="detail"),
 ]
