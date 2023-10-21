0dfd69d58c97c3b301e0b36ad922566c720d1fa0
solaleh81 <solalehalizadeh81.com>
2023-10-21 11:05:41 +0330

    adding login url.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path
 
 from . import views
+
+from .forms import LoginForm
 
 app_name = "core"
 
@@ -9,4 +12,11 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("contact/", views.contact, name="contact"),
     path("signup/", views.signup, name="signup"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="core/login.html", authentication_form=LoginForm
+        ),
+        name="login",
+    ),
 ]
